51084db5d85aaf553c4bdd7db66228d99c77317a
aldaracastromosquera <aldaracastromosquera@gmail.com>
2025-11-11 23:53:53 +0100

    Fase 3: CI/CD GitHub Actions

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,50 +1,36 @@
 #  Variables configurables para personalizar la infraestructura desplegada en Azure.
 
-# ------------------------------------------------------------
-# Nombre del grupo de recursos que contendrá todos los elementos del proyecto en Azure (redes, VM, IPs, etc.)
-# ------------------------------------------------------------
 variable "resource_group_name" {
+  type        = string
   description = "Nombre del grupo de recursos de Azure"
-  default     = "cloudops-rg"
 }
 
-# ------------------------------------------------------------
-# Región o ubicación donde se desplegarán los recursos.
-# ------------------------------------------------------------
 variable "location" {
-  description = "Región donde se desplegarán los recursos"
+  type        = string
+  description = "Región de Azure donde se desplegará la infraestructura"
   default     = "westeurope"
 }
 
-# ------------------------------------------------------------
-# Prefijo utilizado como base para nombrar los recursos.
-# ------------------------------------------------------------
 variable "prefix" {
-  description = "Prefijo base para los nombres de recursos"
+  type        = string
+  description = "Prefijo para nombrar los recursos"
   default     = "cloudops"
 }
 
-# ------------------------------------------------------------
-# Nombre del administrador con el que podrás iniciar sesión en la máquina virtual (por SSH o Azure Portal).
-# ------------------------------------------------------------
+variable "vm_size" {
+  type        = string
+  description = "Tamaño de la máquina virtual"
+  default     = "Standard_B1s"
+}
+
 variable "admin_username" {
-  description = "Usuario administrador para acceder a la VM"
+  type        = string
+  description = "Nombre de usuario administrador de la VM"
   default     = "azureuser"
 }
 
-# ------------------------------------------------------------
-# Contraseña del usuario administrador. Debe cumplir con los requisitos de Azure (mínimo 12 caracteres, mayúsculas, minúsculas, número y símbolo).
-# (!!!) NO dejar contraseñas en texto plano, Se pueden pasar como variables de entorno o desde Azure Key Vault.
-# ------------------------------------------------------------
 variable "admin_password" {
-  description = "Contraseña del usuario administrador (mínimo 12 caracteres)"
-  default     = "CloudOps1234!"
-}
-
-# ------------------------------------------------------------
-# Tipo y tamaño de la máquina virtual a desplegar.
-# ------------------------------------------------------------
-variable "vm_size" {
-  description = "Tamaño de la máquina virtual"
-  default     = "Standard_B1s"
+  type        = string
+  description = "Contraseña del usuario administrador"
+  sensitive   = true
 }
