7704ed05ad9da9f9e1dd095b755f92f273567bc7
aldaracastromosquera <aldaracastromosquera@gmail.com>
2025-11-11 16:46:08 +0100

    Fase 2: Infraestructura Cloud — Terraform (Azure)

--- terraform/aws/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "public_ip" {
-value = aws_instance.app.public_ip
-}
-
-
-output "app_url" {
-value = "http://${aws_instance.app.public_ip}"
-}
--- terraform/aws/variables.tf
+++
@@ -1,6 +0,0 @@
-t_name" { type = string }
-variable "aws_region" { type = string }
-variable "instance_type" { type = string }
-variable "key_name" { type = string }
-variable "github_repo_url" { type = string }
-variable "allowed_cidr" { type = string default = "0.0.0.0/0" }
